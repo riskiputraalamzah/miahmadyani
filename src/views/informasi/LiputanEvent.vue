0c753a873c4f9a4b006c63960f6ddf959050b293
riskiputraalamzah <aryainggil3@gmail.com>
2021-10-14 11:21:46 +0700

    membuat halaman event

---
+++ src/views/informasi/LiputanEvent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-container>
+      <div
+        class="mb-10 text-md-h3 text-sm-h4 text-h5 font-weight-bold text-center "
+      >
+        <span class="font-poppins rule-headline">
+          Liputan Event
+        </span>
+      </div>
+      <section class="py-10"></section>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.isMounted(2);
+  },
+};
+</script>
